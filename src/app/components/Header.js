c64bed542aa5b596528f1b124cf31b0755ccfdbc
Felipe Silva <joaofelipems21@gmail.com>
2023-07-27 05:46:08 -0300

    correção do '

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
             <header>
                 <div className={styles.heros}>
                     <div className={styles.content_title + " " + breeSerif.className}>
-                        <h2 className={styles.subtitle}>I {"'"} m a</h2>
+                        <h2 className={styles.subtitle}>I m a</h2>
                         <h1 className={styles.title}>FULL STACK SOFTWARE DEVELOPER</h1>
                         <Link href="#portfolio" className={styles.others}><button className={styles.button_home}>Projects</button></Link>
                     </div>
